c06a8a07d6ea2bf868690b8e674e30ba570a4559
FabrazJ <m.vargas.or10@gmail.com>
2025-06-08 17:19:44 -0500

    Modifaciones

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-# app.py
-
 from flask import Flask, render_template, request, redirect, url_for
 from product import Product
-from Cart import Cart
+from cart import Cart
 
 app = Flask(__name__)
 carrito = Cart()
@@ -13,24 +11,28 @@ catalogo = {
     "3": Product("3", "Agua", 0.75)
 }
 
-@app.route('/')
-def index():
-    return render_template('index.html', catalogo=catalogo)
+@app.route('/admin')
+def admin():
+    return render_template('Admin/dashboard.html', catalogo=catalogo)
 
-@app.route('/add/<id>')
-def add(id):
+@app.route('/cliente')
+def cliente():
+    return render_template('Client/index.html', catalogo=catalogo)
+
+@app.route('/carrito')
+def carrito_view():
+    return render_template('Client/cart.html', items=carrito.get_items(), total=carrito.total())
+
+@app.route('/agregar/<id>')
+def agregar(id):
     if id in catalogo:
         carrito.add(catalogo[id])
-    return redirect(url_for('index'))
+    return redirect(url_for('cliente'))
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html', items=carrito.get_items(), total=carrito.total())
-
-@app.route('/clear')
-def clear():
+@app.route('/vaciar')
+def vaciar():
     carrito.clear()
-    return redirect(url_for('cart'))
+    return redirect(url_for('carrito_view'))
 
 if __name__ == '__main__':
     app.run(debug=True)
